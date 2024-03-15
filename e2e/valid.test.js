59b2c0e19752e8aae72d0f31e0a371f49d1750ad
OlegKumachev <117755901+OlegKumachev@users.noreply.github.com>
2024-03-15 18:13:52 +0300

    Update valid.test.js

--- e2e/valid.test.js
+++ e2e/valid.test.js
@@ -10,7 +10,7 @@ describe('Card validation functionality', () => {
 
   beforeEach(async () => {
     page = await browser.newPage();
-    await page.goto('http://localhost:8080/');
+    await page.goto('http://localhost:9000/');
   });
 
   afterEach(async () => {
